18fc8768aef69789c4aca4da3c268e9d7386eb09
MBugajski <michalbugajski@outlook.com>
2021-05-14 23:40:15 +0200

    Initial commit - initialized Maven project

---
+++ src/test/java/com/mbugajski/jackson/demo/JacksonDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbugajski.jackson.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
