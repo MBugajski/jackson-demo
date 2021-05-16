f12efe3193f87c7f4fd26b74686ebbdc9c830fbc
MBugajski <michalbugajski@outlook.com>
2021-05-16 23:32:08 +0200

    Added a driver with a main method

---
+++ src/main/java/com/mbugajski/jackson/demo/Driver.java
@@ -0,0 +1,14 @@
+package com.mbugajski.jackson.demo;
+
+public class Driver {
+	public static void main(String[] args) {
+
+		try {
+
+			
+			
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+}
