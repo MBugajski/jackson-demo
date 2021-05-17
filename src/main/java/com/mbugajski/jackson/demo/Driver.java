6f569d37c2e003884137edf1db2c5ca2e121f99a
MBugajski <michalbugajski@outlook.com>
2021-05-17 23:31:08 +0200

    Created a simple object mapper

--- src/main/java/com/mbugajski/jackson/demo/Driver.java
+++ src/main/java/com/mbugajski/jackson/demo/Driver.java
@@ -1,10 +1,21 @@
 package com.mbugajski.jackson.demo;
 
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mbugajski.jackson.demo.entity.Student;
+
 public class Driver {
 	public static void main(String[] args) {
 
 		try {
 
+			ObjectMapper mapper = new ObjectMapper();
+			
+			Student theStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+			
+			System.out.println("First name: " + theStudent.getFirstName());
+			System.out.println("Last name: " + theStudent.getLastName());
 			
 			
 		} catch (Exception exc) {
