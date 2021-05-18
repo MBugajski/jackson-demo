29a764c0d7b1200deea8dc28217a386a48cb33b5
MBugajski <michalbugajski@outlook.com>
2021-05-18 23:54:46 +0200

    Created a driver for binding nested objects

---
+++ src/main/java/com/mbugajski/jackson/demo/DriverNested.java
@@ -0,0 +1,25 @@
+package com.mbugajski.jackson.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mbugajski.jackson.demo.entity.Student;
+
+public class DriverNested {
+	public static void main(String[] args) {
+
+		try {
+
+			ObjectMapper mapper = new ObjectMapper();
+			
+			Student theStudent = mapper.readValue(new File("data/sample-full.json"), Student.class);
+			
+			System.out.println("First name: " + theStudent.getFirstName());
+			System.out.println("Last name: " + theStudent.getLastName());
+			
+			
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+}
