309ef6f44b96ce070c44493ec54f9d38acb059a3
MBugajski <michalbugajski@outlook.com>
2021-05-20 23:50:33 +0200

    Adjusted the main application to display nested data

--- src/main/java/com/mbugajski/jackson/demo/DriverNested.java
+++ src/main/java/com/mbugajski/jackson/demo/DriverNested.java
@@ -3,6 +3,7 @@ package com.mbugajski.jackson.demo;
 import java.io.File;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mbugajski.jackson.demo.entity.Address;
 import com.mbugajski.jackson.demo.entity.Student;
 
 public class DriverNested {
@@ -11,13 +12,18 @@ public class DriverNested {
 		try {
 
 			ObjectMapper mapper = new ObjectMapper();
-			
+
 			Student theStudent = mapper.readValue(new File("data/sample-full.json"), Student.class);
-			
+
 			System.out.println("First name: " + theStudent.getFirstName());
 			System.out.println("Last name: " + theStudent.getLastName());
-			
-			
+
+			Address tempAddress = theStudent.getAddress();
+
+			System.out.println("Street = " + tempAddress.getStreet());
+			System.out.println("City = " + tempAddress.getCity());
+			System.out.println("State = " + tempAddress.getState());
+			System.out.println("Country = " + tempAddress.getCountry());
 		} catch (Exception exc) {
 			exc.printStackTrace();
 		}
