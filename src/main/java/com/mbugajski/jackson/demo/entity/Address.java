909b068815d23bbf42446d72ed66e6896d9e00dc
MBugajski <michalbugajski@outlook.com>
2021-05-22 23:54:26 +0200

    Addnotated entities for Jackson to ignore unknown properties

--- src/main/java/com/mbugajski/jackson/demo/entity/Address.java
+++ src/main/java/com/mbugajski/jackson/demo/entity/Address.java
@@ -1,5 +1,8 @@
 package com.mbugajski.jackson.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Address {
 	private String street;
 	private String city;
--- src/main/java/com/mbugajski/jackson/demo/entity/Student.java
+++ src/main/java/com/mbugajski/jackson/demo/entity/Student.java
@@ -1,5 +1,8 @@
 package com.mbugajski.jackson.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
 	private int id;
 	private String firstName;
